124aca9a075f0d60bec4d949b4cdd9fdeeaa33a3
lucasgkosio <lucas.kosio@hotmail.com>
2026-03-09 11:29:57 -0300

    feat: criamos o botão, capturamos os dados do campo senha, convertemos para String e depois o programa verifica se a senha é par

---
+++ js/login.js
@@ -0,0 +1,17 @@
+ 
+// Capturando os dados do campo SENHA:
+ 
+ 
+const botaoEnviar = document.querySelector("#btn-entrar");
+ 
+botaoEnviar.addEventListener("click", validaCampo);
+ 
+function validaCampo(){
+    const campoSenha = document.getElementById("input-senha");
+    let valorCampo = parseInt(campoSenha.value);
+ 
+    //Verificando se a senha é PAR:
+ 
+    console.log(valorCampo % 2 == 0);
+ 
+}
